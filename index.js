f3812eab18164b52220bf65ea75e60f59ae3e7cc
Maurice Mwangi <mwangimuita18@gmail.com>
2025-06-30 00:21:40 +0300

    sets starter code

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      const idElement = document.createElement('p');
+      idElement.textContent = `New User ID: ${object.id}`;
+      document.body.appendChild(idElement);
+    })
+    .catch(function (error) {
+      const errorElement = document.createElement('p');
+      errorElement.textContent = `Error: ${error.message}`;
+      document.body.appendChild(errorElement);
+    });
+}
